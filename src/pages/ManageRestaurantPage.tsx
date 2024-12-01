40e673e1d8b94ddae92678407afba408c844f8f8
Zümra <zumrakb8@gmail.com>
2024-12-01 23:00:20 +0300

    updating

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,16 +1,23 @@
 import {
   useCreateMyRestaurants,
   useGetMyRestaurants,
+  useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  const { createRestaurant, isLoading } = useCreateMyRestaurants();
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurants();
   const { restaurant } = useGetMyRestaurants(); //iget from get method
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant; //whenever page loads first time,
+
   return (
     <ManageRestaurantForm
-      onSave={createRestaurant}
-      isLoading={isLoading}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading} //anytime we try to create or update
       restaurant={restaurant}
     />
   );
