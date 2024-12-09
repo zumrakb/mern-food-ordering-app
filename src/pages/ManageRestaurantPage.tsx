81ed51869b3d0d0f7dca053f8bf61ca8e217949b
Zümra <zumrakb8@gmail.com>
2024-12-09 16:52:07 +0300

    orders updated

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,25 +1,47 @@
 import {
   useCreateMyRestaurants,
   useGetMyRestaurants,
+  useGetMyRestaurantOrders,
   useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
   const { createRestaurant, isLoading: isCreateLoading } =
     useCreateMyRestaurants();
-  const { restaurant } = useGetMyRestaurants(); //iget from get method
+  const { restaurant } = useGetMyRestaurants();
   const { updateRestaurant, isLoading: isUpdateLoading } =
     useUpdateMyRestaurant();
 
-  const isEditing = !!restaurant; //whenever page loads first time,
+  const { orders } = useGetMyRestaurantOrders();
+
+  const isEditing = !!restaurant;
 
   return (
-    <ManageRestaurantForm
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-      isLoading={isCreateLoading || isUpdateLoading} //anytime we try to create or update
-      restaurant={restaurant}
-    />
+    <Tabs defaultValue="orders" className="max-w-7xl mx-auto">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
